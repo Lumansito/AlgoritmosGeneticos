ee88ced9f121a0b7aef11de3fd5c96872561f558
Lumansito <luhmannm0@gmail.com>
2024-05-12 21:20:32 -0300

    Update TP1.py

--- TP1.py
+++ TP1.py
@@ -27,12 +27,12 @@ def calculoObjetivo():
         binario = ''.join(map(str,individuos[i])) #Convertir el arreglo de digitos binarios en un valor decimal
         numDecimal = int(binario, 2)    
         valObj[i] = funcionObjetivo(numDecimal) #guarda los valores de la func obj de cada indiv
-        print(valObj[i] ,  ' indice: ' , i)
+        #print(valObj[i] ,  ' indice: ' , i)
         totalObj += valObj[i] #acumula la sumatoria de valor obj
     
-    print("###########")
-    print(totalObj)
-    print("######")
+    #print("###########")
+    #print(totalObj)
+    #print("######")
     return totalObj
         
 def funcionFitness():
@@ -42,7 +42,7 @@ def funcionFitness():
         porcFitness[i]=valObj[i]/total
         suma += porcFitness[i]
         
-    print(porcFitness)
+    #print(porcFitness)
 
 def ruleta():
     global individuos
@@ -51,24 +51,63 @@ def ruleta():
     porcentajes =[0 for i in range(1000)] #inicializamos el arreglo de los individuos
     indAct = 0                            #probar con 10 000 para individuos con muy poco fitness
     for i in range(numIndividuos): 
-        fit = int(porcFitness[i]*1000) 
+        fit = int(porcFitness[i]*1000)
         for j in range(fit):
             porcentajes[indAct+j] = i
         indAct = indAct + fit
-    print(indAct)
+    #print(indAct)
     #print(porcentajes)
     for i in range(numIndividuos):
-        num = random.randint(0,indAct) #para no usar 1000 y agregarle posibilidades al ind 0 debido al truncamiento, el arreglo rara vez llega a completar las 1000 posiciones desiganadas
+        if(indAct==1000):   #Sino existia la posibilidad de que el arreglo porcetajes se exeda del limite.
+            indAct=999
+        num = random.randint(0,indAct)#para no usar 1000 y agregarle posibilidades al ind 0 debido al truncamiento, el arreglo rara vez llega a completar las 1000 posiciones desiganadas
+        
         aux = porcentajes[num]
         hijos [i] = individuos[aux] #paso a un array hijos los ganadores
-        print("Gano el individuo: ",aux," ", individuos[aux])
+        #print("Gano el individuo: ",aux," ", individuos[aux])
     individuos = hijos
+
+def mostrarDatosPorCorrida():
+    global menorGlobal, mayorGlobal, valorMayorGlobal, valorMenorGlobal
+    valorMayor=0
+    valorMenor=9999
+    mayor = [0]*numGenes
+    menor = [0]*numGenes
+    total = calculoObjetivo()
+    print("El promedio es: ", total/ numIndividuos)
+    for i in range(numIndividuos):
+        aux = valObj[i]
+        if(aux>valorMayor):
+            valorMayor = aux
+            mayor = individuos[i]
+        if(aux<valorMenor):
+            valorMenor = aux
+            menor = individuos[i]
+            
+        if(aux > valorMayorGlobal):
+            valorMayorGlobal = aux
+            mayorGlobal = individuos[i]
+        if(aux < valorMenorGlobal):
+            valorMenorGlobal = aux
+            menorGlobal = individuos[i]
+    print("El valor maximo es: ", valorMayor , "y su representacion binaria del individuo es: ",mayor)
+    print("El valor menor es: ", valorMenor , "y su representacion binaria del individuo es: ",menor)
+           
+
+           
+def mostrarDatosGlobal():
+    global menorGlobal, mayorGlobal, valorMayorGlobal, valorMenorGlobal
+
+    print("El valor maximo de todas las corridas fue: ", valorMayorGlobal, "y el inndividuo es: ",mayorGlobal )
+    print("El valor menor de todas las corridas fue: ", valorMenorGlobal, "y el inndividuo es: ",menorGlobal )
+
+    
 
 
 def crossover():
-   ind=0
-   for j in range(5):
-        print("entro")
+   ind = 0
+   for j in range(numIndividuos//2):
+        
         aux=random.randint(1,100)
         if(aux<=probCrossover*100):
             corte = random.randint(1,numGenes-1) #punto de corte
@@ -91,22 +130,27 @@ def mutacionBinaria():
 
             posicion = random.randint(0,numGenes-1)
             
-            print("mutuacuin en ",i, "gen", posicion )
-            print(individuos[i])
+            #print("mutuacuin en ",i, "gen", posicion )
+            #print(individuos[i])
             if individuos[i][posicion] == 1:
                 individuos[i][posicion] = 0
             else:
                 individuos[i][posicion] = 1
-            print(individuos[i])
+            #print(individuos[i])
 
 #PROGRAMA PRINCIPAL
 #preguntar el numero de individuos y con cuantos genes 
 
-global numGenes,numIndividuos, individuos, valObj, porcFitness,probCrossover, probMutacion
+global numGenes,numIndividuos, individuos, valObj, porcFitness,probCrossover, probMutacion, valorMayorGlobal, valorMenorGlobal, menorGlobal, mayorGlobal
 numGenes = 30
 numIndividuos = 10
 probCrossover=0.75
-probMutacion=0.01
+probMutacion=0.05
+valorMenorGlobal = 99999
+valorMayorGlobal = 0
+menorGlobal= [0]*30
+mayorGlobal = [0]*30
+
 #Definimos el arreglo de individuos
 individuos = [[0 for i in range(numGenes)] for j in range(numIndividuos)] 
 valObj=[0 for i in range(numIndividuos)]  #para guardar el valor de la func obj de cada individuo 
@@ -116,16 +160,16 @@ porcFitness=[0 for i in range(numIndividuos)] #Para guaradar los valores del por
 #inicializar -> funcion objetivo -> funcion fitness -> seleccion (ruleta) -> crossover (1 punto) -> mutacion 
 
 
-
 #Ejecución
 inicializarPoblacion()
-for i in range(10):
-    print("-----------------",i,"-------------------------")
-    print(individuos)
+for i in range(200):
+    #print("-----------------",i,"-------------------------")
+    #print(individuos)
     funcionFitness()
     ruleta()
+    mostrarDatosPorCorrida()
     crossover()
     mutacionBinaria()
-
-
+    
+mostrarDatosGlobal()
     
