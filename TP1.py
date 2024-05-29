03c26485f91380e1f90d18a561c72656dbc7db7e
LuchoCsd <lucianocasado99@gmail.com>
2024-05-29 12:50:25 -0300

    quitar redundancia y embellecer

--- TP1.py
+++ TP1.py
@@ -1,51 +1,44 @@
 import random
-import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import time
 
 
-def inicializarPoblacion(): #Recorremos el arreglo y lo cargamos con valores Random
-
-    #
+def inicializarPoblacion(): #Recorremos el arreglo y lo cargamos con valores al azar
     global individuos
-    #for i in range(numIndividuos):
-    #    for j in range(numGenes):
-    #       individuos[i][j]= random.randint(0,1)
-
-    individuos = [[1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0], [0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1], [1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 
-0, 0, 0, 1], [1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0], [1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0], [0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 
-0, 1, 0, 0, 1, 1, 1, 0], [0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0], [0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1], [1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 
-0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1], [0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1]]
+    
+    for i in range(numIndividuos):
+        for j in range(numGenes):
+           individuos[i][j]= random.randint(0,1)
 
 
-def funcionObjetivo(x):
+def funcionObjetivo(x): # segun enunciado
     valor= ( (x / (2 ** 30 - 1)) ** 2)
+    
     return valor
 
 
 def calculoObjetivo():
-    totalObj = 0  #total de la funcion objetivo
-
+    totalObj = 0  
+    
     for i in range(numIndividuos):  #para cada individuo
-        binario = ''.join(map(str,individuos[i])) #Convertir el arreglo de digitos binarios en un valor decimal
-        numDecimal = int(binario, 2)    
-        valObj[i] = funcionObjetivo(numDecimal) #guarda los valores de la func obj de cada indiv
-        totalObj += valObj[i] #acumula la sumatoria de valor obj
+        binario = ''.join(map(str,individuos[i])) #Convertir el arreglo de digitos a binario
+        numDecimal = int(binario, 2) #Convertir el binario a decimal
+        valObj[i] = funcionObjetivo(numDecimal) #guarda los valores de la FO de cada individuo respetando su posicion
+        totalObj += valObj[i] #acumula la sumatoria de valores de FO
 
-    return totalObj
+    return totalObj #sumatoria de la funcion objetivo de la generacion
 
 
-def funcionFitness():
-    suma = 0
+def funcionFitness():     
     total = calculoObjetivo()
     
     for i in range(numIndividuos):
-        porcFitness[i]=valObj[i]/total
-        suma += porcFitness[i]
+        porcFitness[i]=valObj[i]/total #Calcula el porcentaje de fitness de cada individuo 
 
 
 def guardarElitismo(n):    #Selecciona los n mejores individuos del arreglo individuos
     global elite
-
     elite = [[] for i in range(n)]
     mayores = [-1 for _ in range(n)]
     indices = [-1 for _ in range (n)]
@@ -66,48 +59,45 @@ def pegarElitismo(n):
 
 def ruleta():
     global individuos
-
     hijos = [[0 for i in range(numGenes)] for j in range(numIndividuos)] #para guardar los hijos
-    porcentajes = [0 for i in range(1000)] #inicializamos el arreglo de los individuos
-    indAct = 0                            #probar con 10 000 para individuos con muy poco fitness
+    porcentajes = [0 for i in range(1000)]  #inicializamos el arreglo de la ruleta
+    indAct = 0 #para llevar la cuenta de las posiciones que se van llenando
     
     for i in range(numIndividuos): 
         fit = int(porcFitness[i]*1000)
         for j in range(fit):
             porcentajes[indAct+j] = i
         indAct = indAct + fit
-
     for i in range(numIndividuos): #numIndividuos veces se hace la ruleta para llenar el resto
-        if(indAct == 1000):   #Sino existia la posibilidad de que el arreglo porcetajes se exeda del limite.
+        if(indAct == 1000):   #para evitar que el arreglo porcetajes se exceda del limite
             indAct = 999
-        num = random.randint(0,indAct)#para no usar 1000 y agregarle posibilidades al ind 0 debido al truncamiento, el arreglo rara vez llega a completar las 1000 posiciones desiganadas
+        num = random.randint(0,indAct) #para no usar 1000 y agregarle posibilidades al ind 0 debido al truncamiento
         aux = porcentajes[num]
-        hijos [i] = individuos[aux].copy() #paso a un array hijos los ganadores
+        hijos [i] = individuos[aux].copy() #paso por valor a un array hijos los ganadores
     individuos = hijos
+
 
 def torneo():
     global individuos
-    N = 4
+    N = 4  #cantidad de individuos a participar de cada torneo segun enunciado
     hijos = [[0 for _ in range(numGenes)]for _ in range(numIndividuos)]
     
     for j in range (numIndividuos):
         candidatos = [0 for i in range(N)] #arreglo de indices de los 4 individuos a examinar
         for i in range(N):
             candidatos[i]=random.randint(0,numIndividuos-1) #Llenar el arreglo de cantidatos con 4 indices al azar
-        mayor = 0 #val FO del mayor indiciduo de los 4
+        mayor = 0 #val FO del mayor individuo de los 4
         indCandidatoFinal=0
         for i in range(N):
             if(valObj[candidatos[i]] > mayor):
                 mayor = valObj[candidatos[i]]
                 indCandidatoFinal = candidatos[i]
-
-        hijos[j]=individuos[indCandidatoFinal].copy()  #Para que pase el valor y no el parametro del arreglo
-        
+        hijos[j]=individuos[indCandidatoFinal].copy()  #selecciona al mejor individuo de los 4 y lo guarda en hijos
     individuos = hijos
+        
 
 def guardarDatosPorCorrida(numCorrida):
     global menorGlobal, mayorGlobal, valorMayorGlobal, valorMenorGlobal, acumCorridas,maximoFo,minimoFo,cromosomasMaximos,cromosomasMinimos, promedioValObjPorCorrida
-    
     valorMayor = 0
     valorMenor = 9999
     mayor = [0]*numGenes
@@ -137,19 +127,10 @@ def guardarDatosPorCorrida(numCorrida):
     minimoFo[numCorrida] = valorMenor
     cromosomasMaximos[numCorrida] = mayor
     cromosomasMinimos[numCorrida] = menor
-        
-def mostrarDatosGlobal():
-    global valorMayorGlobal, valorMenorGlobal, mayorGlobal, menorGlobal
-
-    print("El valor máximo de todas las corridas fue: ", valorMayorGlobal, "y el individuo es: ", mayorGlobal)
-    print("El valor mínimo de todas las corridas fue: ", valorMenorGlobal, "y el individuo es: ", menorGlobal)
-
-
 
 
 def crossover():
    ind = 0
-
    for j in range(numIndividuos//2):
         aux=random.randint(1,100)
         if(aux<=probCrossover*100):
@@ -165,56 +146,41 @@ def mutacionBinaria():
     for i in range(numIndividuos):
         if(random.randint(0,100)<=probMutacion*100):
             posicion = random.randint(0,numGenes-1)
-            print("\033[91mMUTACION en individuo \033[0m", i," en pos ", posicion)
             if individuos[i][posicion] == 1:
                 individuos[i][posicion] = 0
             else:
                 individuos[i][posicion] = 1
-            
 
-def dibujarGrafico():
 
-    global promedioValObjPorCorrida, corridas
-
-    numCorridas=[0 for _ in range(corridas)]
-
-    for i in range(corridas):
-        numCorridas[i] = i+1
-        
-    # Crear el gráfico de líneas
-    plt.plot(numCorridas, promedioValObjPorCorrida)
-
-    # Personalización  
-    plt.title('Gráfico de Líneas')
-    plt.xlabel('Numero de corridas')
-    plt.ylabel('Promedio de cada corrida')
-    # Mostrar el gráfico
-    plt.show()
+def nombrarArchivo(op,corridas):
+    if op == "R":
+        return "Ruleta_" + str(corridas)+".xlsx"
+    elif op == "T":
+        return "Torneo_" + str(corridas)+".xlsx"
+    elif op == "RE":
+        return "RuletaConElitismo_" + str(corridas)+".xlsx"
 
 
 def realizarTabla():
-    global individuos,valObj,porcFitness, nombreArchivoExcel
-    
+    global individuos,valObj,porcFitness
     datos_tabla = {
-        "Generacion" : [i+1 for i in range(corridas)],
-        "Minimo FO": minimoFo,
-        "Maximo FO": maximoFo,
-        "Cromosoma Maximo(binario)":cromosomasMaximos,
-        "Cromosoma Minimo(binario)": cromosomasMinimos,
+        "Generación" : [i+1 for i in range(corridas)],
+        "Mínimo FO": minimoFo,
+        "Máximo FO": maximoFo,
+        "Cromosoma Máximo(binario)":cromosomasMaximos,
+        "Cromosoma Mínimo(binario)": cromosomasMinimos,
         "Promedio":promedioValObjPorCorrida
     }
     df_individuos = pd.DataFrame(datos_tabla)
-    
-    nombreArchivoExcel = nombreArchivoExcel + ".xlsx"
     with pd.ExcelWriter(nombreArchivoExcel) as writer:
         df_individuos.to_excel(writer, sheet_name = 'Individuos', index = False)
 
 
 #PROGRAMA PRINCIPAL
-#preguntar el numero de individuos y con cuantos genes 
+ 
 
 global numGenes,numIndividuos, individuos, valObj, porcFitness,probCrossover, probMutacion, valorMayorGlobal, valorMenorGlobal, menorGlobal, mayorGlobal, corridas, promedioValObjPorCorrida, nombreArchivoExcel
-numGenes = 30
+numGenes = 30 #Estan definidos por el enunciado, pero se puede pedir al usuario
 numIndividuos = 10
 probCrossover = 0.75
 probMutacion = 0.05
@@ -223,11 +189,11 @@ valorMayorGlobal = 0
 menorGlobal = [0]*numGenes
 mayorGlobal = [0]*numGenes
 corridas = 0 #Este valor es modificado en la ejecucion
+
 #Definimos el arreglo de individuos
 individuos = [[0 for i in range(numGenes)] for j in range(numIndividuos)] 
 valObj = [0 for i in range(numIndividuos)]  #para guardar el valor de la func obj de cada individuo 
 porcFitness = [0 for i in range(numIndividuos)] #Para guaradar los valores del porcentaje que brinda la fun fitness para cada indiviuo
-
 
 
 #Incializar los valores que dependen de las corridas
@@ -239,36 +205,25 @@ def inicializarValoresCorridas():
     maximoFo = [0 for _ in range(corridas)]
     cromosomasMaximos = [0 for _ in range(corridas)]
     cromosomasMinimos = [0 for _ in range(corridas)]
-    #para graficos
     promedioValObjPorCorrida = [0 for _ in range(corridas)]
-#OPCION A:
-#inicializar -> funcion objetivo -> funcion fitness -> seleccion (ruleta) -> crossover (1 punto) -> mutacion 
 
-#OPCION B:
-#inicializar -> funcion objetivo -> funcion fitness -> seleccion (torneo) -> crossover (1 punto) -> mutacion
-#TORNEO = 10 veces (se seleccionan 4 individuos al azar y pasa el que mejor fitness tiene)
-
-#OPCION C:
-#inicializar -> funcion objetivo -> funcion fitness -> elitismo (pasan 2 derecho)-> 
-#seleccion (ruleta entre los 10, para seleccionar los 8 restantes) -> crossover (1 punto) -> mutacion 
-
-
-
-#Ejecución Opcion A
 
 op = ""
-op = input( "Ingrese la opcion de seleccion \n R-Ruleta\n T- Torneo\n RE- Ruleta-Elitismo\n F - para terminar\n").upper()
+print("_"*90)
+op = input("   R - Ruleta\n   T - Torneo\n   RE - Ruleta con Elitismo\n   F - Salir\nIngrese el método de selección deseado: ").upper()
 while op.upper() != "R" and op.upper() != "T" and op.upper() != "RE" and op.upper() != "F":
     print("Opcion no valida")
-    op = input( "Ingrese la opcion de seleccion \n R-Ruleta\n T- Torneo\n RE- Ruleta-Elitismo\n F - para terminar\n").upper()
+    op = input("   R - Ruleta\n   T - Torneo\n   RE - Ruleta con Elitismo\n   F - Salir\nIngrese el método de selección deseado: ").upper()
 
 while op.upper() !="F":
-    nombreArchivoExcel = input("Ingrese el nombre de la tabla a exportar:\n")
-    corridas = int(input("Ingrese el numero de corridas que desea Realizar: \n"))
+    corridas = int(input("Ingrese el numero de corridas que desea Realizar: "))
+    nombreArchivoExcel = nombrarArchivo(op,corridas)
+    current_directory = os.getcwd()
+    time.sleep(1)
+    print("\nLos resultados se encuentran en: \033[92m",current_directory,"\033[0m\nBajo el nombre de: \033[92m",nombreArchivoExcel+"\033[0m")
     inicializarValoresCorridas()
     inicializarPoblacion()
     for i in range(corridas):
-        print("-----------------",i,"-------------------------")
         funcionFitness()
         guardarDatosPorCorrida(i)
         if (op == "R"):
@@ -282,15 +237,12 @@ while op.upper() !="F":
         mutacionBinaria()
         if (op == "RE"):
             pegarElitismo(2)
-            
-    mostrarDatosGlobal()
-    dibujarGrafico()
     realizarTabla()
-    op = input( "Ingrese la opcion de seleccion \n R-Ruleta\n T- Torneo\n RE- Ruleta-Elitismo\n F - para terminar\n").upper()
+    time.sleep(4)
+    print("_"*90+"\n")
+    print("Desea realizar una nueva corrida?")
+    op = input("   R - Ruleta\n   T - Torneo\n   RE - Ruleta con Elitismo\n   F - Salir\nIngrese el método de selección deseado: ").upper()
     while op.upper() != "R" and op.upper() != "T" and op.upper() != "RE" and op.upper() != "F":
-        print("Opcion no valida")
-        op = input( "Ingrese la opcion de seleccion \n R-Ruleta\n T- Torneo\n RE- Ruleta-Elitismo\n F - para terminar\n").upper()
-
-
-
-
+        print("Opción no valida")
+        op = input("   R - Ruleta\n   T - Torneo\n   RE - Ruleta con Elitismo\n   F - Salir\nIngrese el método de selección deseado: ").upper()
+print("\n--- Fin del programa ---")
