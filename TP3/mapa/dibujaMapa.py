cbb13c82c83e4e8f4bcfa7d3b83ebda531905d31
Vittorio Maragliano <maraglianovittorio@gmail.com>
2024-09-30 09:05:04 -0300

    arreglo coordenadas de mapas

--- TP3/mapa/dibujaMapa.py
+++ TP3/mapa/dibujaMapa.py
@@ -6,28 +6,28 @@ import networkx as nx
 capitales = {
     'Cdad. de Bs. As.': (-58.3772, -34.6131),
     'Córdoba': (-64.1888, -31.4201),
-    'Corrientes': (-58.8341, -27.4692),
+    'Corrientes': (-57.8341, -27.9692),
     'Formosa': (-58.1781, -26.1775),
     'La Plata': (-57.9545, -34.9214),   
     'La Rioja': (-66.8557, -29.4135),
     'Mendoza': (-68.8458, -32.8895),
-    'Neuquén': (-68.0583, -38.9517),
-    'Paraná': (-60.5363, -31.7413),
+    'Neuquén': (-68.9583, -38.9517),
+    'Paraná': (-59.52, -31.7413),
     'Posadas': (-55.8929, -27.3646),
     'Rawson': (-65.3085, -43.2527),
     'Resistencia': (-58.9906, -27.4606),
-    'Río Galleqos': (-68.3105, -51.6234),
+    'Río Galleqos': (-69.3105, -51.6234),
     'S.F.d.V.d. Catamarca': (-65.7851, -28.4696), 
     'S.M. de Tucumán': (-65.2226, -26.8241),  
     'S.S. de Jujuy': (-65.2971, -24.1946),    
     'Salta': (-65.4117, -24.7821),
     'San Juan': (-68.5367, -31.5375),
     'San Luis': (-66.3378, -33.3017),
-    'Santa Fe': (-60.7013, -31.6230),
+    'Santa Fe': (-60.7733, -31.6230),
     'Santa Rosa': (-64.2838, -36.6202),
     'Sgo. Del Estero': (-64.2642, -27.7845),  
     'Ushuaia': (-68.3040, -54.8050),
-    'Viedma': (-67.5824, -39.0339)
+    'Viedma': (-64.2143, -40.58)
 }
 
 # Lista de localidades en el mismo orden que los índices
@@ -82,7 +82,7 @@ def dibujarMapa(orden):
     nx.draw_networkx_nodes(G, pos, ax=ax, node_size=10, node_color='red')
 
     # Etiquetas de las capitales
-    nx.draw_networkx_labels(G, pos, ax=ax, font_size=10, font_color='black')
+    nx.draw_networkx_labels(G, pos, ax=ax, font_size=5, font_color='black')
 
     # Mostrar el mapa
     plt.title("Recorrido realizado")
